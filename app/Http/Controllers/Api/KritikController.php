8d49271ab7d462c71779f30d2d79822e86d34348
unknown <icalkomp@gmail.com>
2026-06-09 12:10:26 +0700

    update

---
+++ app/Http/Controllers/Api/KritikController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\KritikSaran; // Panggil model lu
+use Illuminate\Http\Request;
+
+class KritikController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_pengirim' => 'required|string',
+            'isi_pesan' => 'required|string',
+        ]);
+
+        KritikSaran::create([
+            'nama_pengirim' => $request->nama_pengirim,
+            'isi_pesan'     => $request->isi_pesan,
+            'tanggal_kirim' => now(), // Sesuaikan dengan nama field di model lu
+            'status'        => 'Baru' // Default status
+        ]);
+
+        return response()->json(['sukses' => true, 'pesan' => 'Terima kasih atas masukannya!'], 200);
+    }
+}
