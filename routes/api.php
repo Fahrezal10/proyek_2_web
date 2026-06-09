f617deef64170fd422a5173cc0a746d91cbefbaa
unknown <icalkomp@gmail.com>
2026-06-09 12:22:37 +0700

    update lagi

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\Api\KegiatanApiController;
 use App\Http\Controllers\Api\TransaksiApiController;
 use App\Http\Controllers\Api\DonasiApiController;
+use App\Http\Controllers\Api\KritikController;
 
 Route::get('/kegiatan', [KegiatanApiController::class, 'getInformasi']);
 Route::get('/transaksi', [TransaksiApiController::class, 'getTransaksi']);
 Route::post('/donasi', [DonasiApiController::class, 'prosesDonasi']);
+Route::post('/kritik', [KritikController::class, 'store']);
